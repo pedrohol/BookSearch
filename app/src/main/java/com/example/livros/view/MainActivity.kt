4ad97484abc6ab216737967d373ccc06531295ca
pedrohol <pethusok@gmail.com>
2025-12-22 12:14:47 -0300

    Search Book Screen

--- app/src/main/java/com/example/livros/view/MainActivity.kt
+++ app/src/main/java/com/example/livros/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.livros
+package com.example.livros.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.livros.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
